9ff17d7202f13e4de51279bfee2960f927f1d3b2
Osián <149536063+OsianJL@users.noreply.github.com>
2024-11-20 18:57:55 +0000

    classes and constructor

--- main.js
+++ main.js
@@ -21,10 +21,43 @@ const users1 = [];
 const users2 = [];
 const newUser = {
     id: 4,
-    name: 'pedro',
+    name: 'pedro', // puedo incluir una propiedad nueva, pero no podría si newUser fuera : User.
     username: 'osi',
     email: 'osi@gmail.com'
 };
 users1.push(newUser);
 //* cuando uso cualquier metodo de js, me ayuda con el tipado de cada cosa
 const arrayFiltrado = users1.filter((user) => { user.username.includes('i'); });
+const perro1 = {
+    name: 'toby',
+    raza: 'caniche',
+    gender: 'male'
+};
+const pajaro1 = {
+    name: 'toby',
+    raza: 'caniche',
+    gender: 'male'
+};
+const fish1 = {
+    name: 'toby',
+    raza: 'caniche',
+    gender: 'male',
+    itSwims: true
+};
+class Coche {
+    constructor(marca, modelo, puertas) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.puertas = puertas;
+    }
+    info() {
+        console.log(`es un coche ${this.marca} ${this.modelo} y tiene ${this.puertas} puertas`);
+    }
+    getMarca() {
+        return this.marca;
+    }
+    setMarca(nuevaMarca) {
+        this.marca = nuevaMarca;
+    }
+}
+const c1 = new Coche('opel', 'corsa', 3);
--- main.ts
+++ main.ts
@@ -54,13 +54,107 @@ const users2: User[] = []
 
 const newUser = {
     id: 4, 
-    name: 'pedro',
+    name: 'pedro', // puedo incluir una propiedad nueva, pero no podría si newUser fuera : User.
     username: 'osi',
     email: 'osi@gmail.com'
 }
 
-users1.push(newUser as User)
+users1.push(newUser)
 
 //* cuando uso cualquier metodo de js, me ayuda con el tipado de cada cosa
 
 const arrayFiltrado = users1.filter((user) => {user.username.includes('i')})
+
+
+//*================================================
+//* type--- permite generar nuevos tipos de datos simples. no hace falta que sean objetos
+
+type Gender = 'male' | 'female' | 'non-binary'
+
+
+interface Perro {
+    name: string;
+    raza: string; 
+    gender: 'male' | 'female';
+}
+interface Gato {
+    name: string;
+    raza: string; 
+    gender: Gender
+}
+
+
+const perro1: Perro = {
+    name: 'toby',
+    raza: 'caniche',
+    gender: 'male'
+}
+//* tambien puedes crear tipos de objeto usando type
+
+type Pajaro = {
+    name: string;
+    raza: string; 
+    gender: Gender
+}
+
+const pajaro1: Pajaro = {
+    name: 'toby',
+    raza: 'caniche',
+    gender: 'male'
+}
+
+//* tambien podemos extender types, usamos ----> 
+
+type Pescado = Pajaro & {
+    itSwims: boolean
+}
+
+const fish1: Pescado = {
+    name: 'toby',
+    raza: 'caniche',
+    gender: 'male',
+    itSwims: true
+}
+
+
+//*================================================
+//*================================================
+//* Clases
+
+interface ClassCoche {
+    marca: string; 
+    modelo: string; 
+    puertas: number; 
+    info: () => void; // una funcion queno recibe parametros y no retorna nada
+    getMarca: () => string;
+    setMarca: (nuevaMarca: string) => void 
+
+}
+
+class Coche implements ClassCoche {
+    //! es obligatorio definir el tipo de cada propiedad antes del constructor
+    marca: string; 
+    modelo: string; 
+    puertas: number; 
+    constructor(marca: string, modelo: string, puertas: number) {
+        this.marca = marca
+        this.modelo = modelo 
+        this.puertas = puertas
+    }
+
+    info () {
+        console.log(`es un coche ${this.marca} ${this.modelo} y tiene ${this.puertas} puertas`);
+        
+    }
+
+    getMarca () {
+        return this.marca
+    }
+
+    setMarca (nuevaMarca: string)  {
+        this.marca = nuevaMarca; 
+    }
+
+}
+
+const c1 = new Coche('opel', 'corsa', 3)
